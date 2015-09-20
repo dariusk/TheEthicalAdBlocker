124f8398c3d93ece2eb0f2237a9eff14f49bc55f
Darius Kazemi <darius.kazemi@gmail.com>
2015-09-20 04:54:05 -0700

    change copy and fix licensing stuff

--- message.js
+++ message.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of The Ethical Ad Blocker,
+ * Copyright (C) 2006-2015 Eyeo GmbH
+ *
+ * The Ethical Ad Blocker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * The Ethical Ad Blocker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Ethical Ad Blocker.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 Array.prototype.pickRemove = function() {
   var index = Math.floor(Math.random()*this.length);
   return this.splice(index,1)[0];
